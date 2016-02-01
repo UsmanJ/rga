a01a5c5a4539bd08324729437a5e123e712656c4
Usman Jamil <usman.jamil@live.co.uk>
2016-02-01 22:19:25 +0000

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rgaAssessmentApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the rgaAssessmentApp
+ */
+angular.module('rgaAssessmentApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rgaAssessmentApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rgaAssessmentApp
+ */
+angular.module('rgaAssessmentApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
