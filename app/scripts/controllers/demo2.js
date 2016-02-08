5c054563ca3473b1c216ad93987b915c9dddfee1
Usman Jamil <usman.jamil@live.co.uk>
2016-02-08 20:00:34 +0000

    working on task 2 - yet to display results and remove form

--- app/scripts/controllers/demo2.js
+++ app/scripts/controllers/demo2.js
@@ -5,7 +5,34 @@ angular.module('rgaAssessmentApp')
 
     var self = this;
 
-    self.gender = [ "male", "female" ];
-    self.percentage = [ 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 ]
+    self.data = { 'Male': '', 'Female': '' };
+    self.dataAvailable2 = false;
+    self.dataAvailable1 = false;
+
+
+    self.add = function() {
+      var percentage = document.getElementById('percentageValue');
+      var percentageValue = percentage.options[percentage.selectedIndex].value;
+
+      if (genderValue === 'Male') {
+        self.data['Male'] = percentageValue;
+        self.maleAvailable = true;
+        console.log(self.data['Male'])
+      } else if (genderValue === 'Female') {
+        self.data['Female'] = percentageValue;
+        self.femaleAvailable = true;
+      }
+    };
+
+    self.remove = function(data) {
+
+      if (genderValue === 'Male') {
+        self.data['Male'] = percentageValue;
+        self.dataAvailable = true;
+      } else if (genderValue === 'Female') {
+        self.data['Female'] = percentageValue;
+        self.dataAvailable = true;
+      }
+    };
 
 }]);
